113f4d48b6c8053f8b3ba90ef88ec0550e1886d9
Joshysmart <sanijoshua10@gmail.com>
2020-09-09 05:49:44 +0100

    Updates

---
+++ precache-manifest.18fcc697ae42d27c0e2fea6f658a30a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9615757e9ae222f02b139773923a33d2",
+    "url": "/static-job-listings/index.html"
+  },
+  {
+    "revision": "6df347354a1e430ec690",
+    "url": "/static-job-listings/static/css/main.0f6045e8.chunk.css"
+  },
+  {
+    "revision": "d0b43cc960f6a36b4765",
+    "url": "/static-job-listings/static/js/2.b8fb0617.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static-job-listings/static/js/2.b8fb0617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df347354a1e430ec690",
+    "url": "/static-job-listings/static/js/main.9712d758.chunk.js"
+  },
+  {
+    "revision": "fee4e05dc36b6f179ffe",
+    "url": "/static-job-listings/static/js/runtime-main.94596790.js"
+  },
+  {
+    "revision": "611ef8c01981f61c6d88420bf213dde1",
+    "url": "/static-job-listings/static/media/cursor.611ef8c0.svg"
+  }
+]);
